3a973f8779748052680b6af258e51ec641b3ba65
= <=>
2023-08-25 10:10:55 +0200

    first commit

---
+++ areal.js
@@ -0,0 +1,20 @@
+"use strict";
+
+rektangelArea();
+
+function rektangelArea(height, width) {
+  return height * width;
+}
+console.log(rektangelArea(30, 20));
+
+//function solve(num, per = 25) {
+//  console.log((num * per) / 100 + num);
+//}
+
+kvadrattilsquare();
+
+function kvadrattilsquare(m2) {
+  return m2 * 10.76;
+}
+
+console.log(`der er ${kvadrattilsquare(20)}ft2 pr 20m2`);
